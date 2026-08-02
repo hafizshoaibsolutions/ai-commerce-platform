3df45310ddff33a0c19148bb0b7859ca76aa337e
hafizshoaibsolutions <hafizshoaibsuleman@gmail.com>
2026-08-02 14:34:30 +0500

    feat(auth): add authentication middleware

---
+++ backend/src/middlewares/auth.middleware.ts
@@ -0,0 +1,48 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+import User from "../models/user.model";
+import AppError from "../utils/app-error.util";
+
+interface jwtPayload {
+  userId: string;
+}
+
+export const authenticate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      throw new AppError("Authorization header missing or malformed", 401);
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.ACCESS_TOKEN_SECRET!,
+    ) as jwtPayload;
+
+    const user = await User.findById(decoded.userId).select(
+      "-password -refreshTokens",
+    );
+
+    if (!user) {
+      throw new AppError("User not found", 404);
+    }
+
+    if (!user.isActive) {
+      throw new AppError("User account is inactive", 403);
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
